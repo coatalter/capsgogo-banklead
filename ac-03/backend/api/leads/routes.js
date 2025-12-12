5acb05df6f57307534c6549483a2a1872228250a
coatalter <111971202+coatalter@users.noreply.github.com>
2025-12-12 13:31:16 +0700

    Add files via upload

--- ac-03/backend/api/leads/routes.js
+++ ac-03/backend/api/leads/routes.js
@@ -14,6 +14,27 @@ const routes = (handler) => [
     path: '/leads-stats',
     handler: handler.getLeadsStatsHandler,
   },
+  {
+    method: 'PUT', // Route Update
+    path: '/leads/{id}',
+    handler: handler.editLeadStatusHandler,
+    options: {
+      payload: {
+        allow: 'application/json',
+        parse: true
+      }
+    }
+  },
+  {
+    method: 'GET', // Route Logs
+    path: '/logs',
+    handler: handler.getLogsHandler,
+  },
+  {
+    method: 'GET', // Route Leaderboard
+    path: '/leaderboard',
+    handler: handler.getLeaderboardHandler,
+  },
 ];
 
 module.exports = routes;
